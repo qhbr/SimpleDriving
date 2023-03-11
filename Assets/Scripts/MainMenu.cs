53eef86a383d956159ebd0a6206c746b4b2e4f95
qhbr <hihabibihere@hotmail.com>
2023-03-11 23:27:15 +0800

    HighScore
    Added HighScore text  and codes to save highscore and

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_Text HighScoreText;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
+
+        HighScoreText.text = $"High Score: {highScore}";
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
