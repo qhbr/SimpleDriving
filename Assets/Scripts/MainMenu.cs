b971bcf173818b42dcdaceebe3a1eceabdecb433
qhbr <hihabibihere@hotmail.com>
2023-03-12 00:25:32 +0800

    Energy System
    code amd text for energy system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,17 +8,57 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text HighScoreText;
+    [SerializeField] private TMP_Text EnergyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
         HighScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if(energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+            
+            if(energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if(DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+
+        }
+
+        EnergyText.text = $"Play ({energy})";
         
     }
 
     public void Play()
     {
+        if(energy < 1) { return; }
+
+        energy--;
+
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if(energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
     }
 
